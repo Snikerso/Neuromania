e613d840908843ef487a60f9a0296d77c3b4447d
Snikerso <42245963+Snikerso@users.noreply.github.com>
2021-10-31 08:22:44 +0100

    payment info

--- pages/registration.js
+++ pages/registration.js
@@ -19,6 +19,10 @@ width: 100vw;
         align-items: flex-start;
     }
 `
+const WrapperPaymentInfo = styled.div`
+margin:20px 0;
+
+`
 
 const StyledWrapperAccordion = styled.div`
     display:flex;
@@ -42,12 +46,26 @@ export default function VenuePage() {
         <li>50 zł/12 € - passive participant</li>
         <li>80 zł/18 € - active participant</li>
       </ul>
+       
       <Paragraph text={`Please fill out the form below to register for the conference `}/>
         <Link isImportant target={"_blank"} href={"https://forms.gle/WvqBYPtSnmE5GP5A7"} text={"https://forms.gle/WvqBYPtSnmE5GP5A7"}/>
         <Link target={"_blank"} href={"/assets/files/Regulations.pdf"} text={"Regulations of the Scientific Conference"}/>
         <Link target={"_blank"} href={"/assets/files/Rodo_eng.pdf"} text={"Information on the processing of personal data"}/>
         <Link target={"_blank"} href={"/assets/files/Rodo_pl.pdf"} text={"Informacja o przetwarzaniu danych osobowych"}/>
+        
+            <WrapperPaymentInfo>
+          <Paragraph text={`Participation fees ought to be transferred to the following bank account:`}/>
+          <Paragraph text={`Uniwersytet Mikołaja Kopernika w Toruniu `}/>
+          <Paragraph text={`Bank Millenium S.A. Warszawa `}/>
+          <Paragraph text={`45 1160 2202 0000 0000 3174 8579 `}/>
+          <Paragraph text={`With an addition: neuromania, a participant's full name.   `}/>
+          <Paragraph text={`Foreign transfers should also include:`}/>
+          <Paragraph text={`SWIFT: BIGBPLPWXXX `}/>
+          <Paragraph text={`IBAN: PL45 1160 2202 0000 0000 3174 8579`}/>
+    </WrapperPaymentInfo>
         <StyledWrapperAccordion>
+
+          
         <h2>Important Dates</h2>
         <Accordion.Wizzard>
             {importantDatesData.map(item =><Accordion.Item  key={item.id} item={item}/> )}
